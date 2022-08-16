36a097f2911b39871461b41e3e13fb68d2a2647e
Palloma Hortencio <palloma.hortencio@gmail.com>
2022-08-16 11:53:29 -0300

    Usando o comando chalk

--- exemplo5/index.js
+++ exemplo5/index.js
@@ -1,11 +1,14 @@
 import chalk from 'chalk';
 
+// para instalar o chalk utilizamos o comando npm instaal chalk
+// chalk é um dos muitos pacotes disponíveis no repositório npmjs.com
+
 let aluno = "Palloma";
 let idade = 21;
 
-console.log(chalk.bgRed('Trabalhando com NPM'));
-if(idade => 18) {
-    console.log(`${aluno} está aprovado!`);
+console.log(chalk.red('Trabalhando com NPM'));
+if(idade > 18) {
+    console.log(`${aluno} ${chalk.magenta ('está aprovada')}`);
 } else {
-    console.log(`${aluno} está reprovado!`);
+    console.log(`${aluno} ${chalk.yellow ('está reprovada')}`);
 }
